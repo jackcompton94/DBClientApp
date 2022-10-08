7c5f6f487afe4e1a815fdf9a30485d68f8f0952f
Jack Compton <jackcompton94@gmail.com>
2022-10-08 17:08:02 -0600

    scheduler testing v1

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -1,14 +1,18 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
-
+public class MainMenuController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("successfully loaded controller");
+    }
+
+    public void onActionSignIn(ActionEvent actionEvent) {
+        System.out.println("sign in button pressed");
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/MainMenu.fxml"));
         primaryStage.setTitle("Scheduler");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 540, 395));
         primaryStage.show();
     }
 
