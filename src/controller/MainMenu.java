5fe30b85ed759aac42376bca9680cfb260fe671d
Jack Compton <jackcompton94@gmail.com>
2022-10-08 23:43:27 -0600

    scheduler testing v1

--- src/controller/MainMenu.java
+++ src/controller/MainMenu.java
@@ -5,15 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,13 +26,33 @@ public class MainMenu implements Initializable {
     @FXML
     private PasswordField passwordText;
 
+    @FXML
+    private Label invalidLogin;
+
+
+    // learn CSS events in Java
+    // learn Zone ID & language awareness
 
     public void onActionSignIn(ActionEvent actionEvent) {
-        System.out.println("sign in button pressed");
+        if ((userIdText.getText().equals("sqlUser")) && (passwordText.getText().equals("Passw0rd!"))) {
+            System.out.println("valid credentials");
+            invalidLogin.setVisible(false);
+
+            //TODO: insert next scene
+        }
+        else if ((userIdText.getText().isEmpty()) || passwordText.getText().isEmpty()) {
+            System.out.println("need credentials");
+            invalidLogin.setVisible(true);
+        }
+        else {
+            System.out.println("invalid credentials");
+            invalidLogin.setVisible(true);
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("successfully loaded controller");
+        invalidLogin.setVisible(false);
     }
 }
