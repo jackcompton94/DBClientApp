d7b9a0917d826021cb6fe9b8b191582d24368f30
Jack Compton <jackcompton94@gmail.com>
2022-10-10 11:35:48 -0600

    scheduler v1.2

---
+++ src/controller/Appointments.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Appointments implements Initializable {
+
+    Parent scene;
+
+    public void back(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
--- src/controller/MainMenu.java
+++ src/controller/MainMenu.java
@@ -2,21 +2,36 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenu implements Initializable {
 
-    public void viewAppointments(ActionEvent actionEvent) {
-        System.out.println("view appointments");
+    Stage stage;
+    Parent scene;
+
+    public void viewAppointments(ActionEvent actionEvent) throws IOException {
+        stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/Appointments.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
-    public void viewCustomers(ActionEvent actionEvent) {
-        System.out.println("view customers");
+    public void viewCustomers(ActionEvent actionEvent) throws IOException {
+        stage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/Customers.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @Override
