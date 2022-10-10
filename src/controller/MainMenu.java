bffac6a29a4e1bf6667fd901105320ddc0a16f44
Jack Compton <jackcompton94@gmail.com>
2022-10-10 11:15:51 -0600

    scheduler v1.2

--- src/controller/MainMenu.java
+++ src/controller/MainMenu.java
@@ -1,15 +1,26 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javax.swing.*;
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenu implements Initializable {
 
+    public void viewAppointments(ActionEvent actionEvent) {
+        System.out.println("view appointments");
+    }
+
+    public void viewCustomers(ActionEvent actionEvent) {
+        System.out.println("view customers");
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 }
+
