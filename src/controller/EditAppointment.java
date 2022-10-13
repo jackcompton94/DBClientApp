b44b76e92703f0588b09e426ffd5eac6138f5296
Jack Compton <jackcompton94@gmail.com>
2022-10-13 13:20:13 -0600

    scheduler - Edit Appointments (Date / Time)

--- src/controller/EditAppointment.java
+++ src/controller/EditAppointment.java
@@ -1,5 +1,10 @@
 package controller;
 
+import databaseAccess.accessContacts;
+import databaseAccess.accessCustomers;
+import databaseAccess.accessUsers;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -7,6 +12,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.util.converter.LocalDateTimeStringConverter;
 import model.Appointment;
 import model.Contact;
 import model.Customer;
@@ -14,59 +20,102 @@ import model.User;
 import org.w3c.dom.Text;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class EditAppointment implements Initializable {
 
     @FXML
-    public TextField appointmentId;
+    private TextField appointmentId;
 
     @FXML
-    public TextField title;
+    private TextField title;
 
     @FXML
-    public TextField description;
+    private TextField description;
 
     @FXML
-    public TextField location;
+    private TextField location;
 
     @FXML
-    public TextField type;
+    private TextField type;
 
     @FXML
-    public DatePicker date;
+    private DatePicker date;
 
     @FXML
-    public ComboBox<Integer> startHour;
+    private ComboBox<Integer> startHour;
 
     @FXML
-    public ComboBox<String> startMinute;
+    private ComboBox<String> startMinute;
 
     @FXML
-    public ComboBox<Integer> endHour;
+    private ComboBox<Integer> endHour;
 
     @FXML
-    public ComboBox<String> endMinute;
+    private ComboBox<String> endMinute;
 
     @FXML
-    public ComboBox<Contact> contact;
+    private ComboBox<Contact> contact;
 
     @FXML
-    public ComboBox<Customer> customer;
+    private ComboBox<Customer> customer;
 
     @FXML
-    public ComboBox<User> user;
+    private ComboBox<User> user;
 
-    public void sendAppointment(Appointment selectedItem) {
-        appointmentId.setText(String.valueOf(Appointment.getAppointmentId()));
-        title.setText(Appointment.getTitle());
+    public void initTime() {
+        ObservableList<Integer> hourTimes = FXCollections.observableArrayList();
+        for (int i = 1; i <= 24; i++) {
+            hourTimes.add(i);
+        }
+        startHour.setItems(hourTimes);
+        endHour.setItems(hourTimes);
 
-
-
+        ObservableList<String> minuteTimes = FXCollections.observableArrayList("00", "15", "30", "45");
+        startMinute.setItems(minuteTimes);
+        endMinute.setItems(minuteTimes);
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void sendAppointment(Appointment appointment) {
+        appointmentId.setText(String.valueOf(appointment.getAppointmentId()));
+        title.setText(appointment.getTitle());
+        description.setText(appointment.getTitle());
+        location.setText(appointment.getLocation());
+        type.setText(appointment.getType());
+
+        //date
+
+        //startHour
+        //startMinute
+        //endHour
+        //endMinute
+
+        //contactID
+        int contactId = appointment.getContactId();
+        for (Contact c : accessContacts.getAllContacts()) {
+            if (contactId == c.getContactId()){
+                contact.setValue(c);
+            }
+        }
+
+        //customerID
+        int customerId = appointment.getCustomerId();
+        for (Customer c : accessCustomers.getAllCustomers()) {
+            if (customerId == c.getCustomerId()) {
+                customer.setValue(c);
+            }
+        }
+
+        //userID
+        int userId = appointment.getUserId();
+        for (User u : accessUsers.getAllUsers()) {
+            if (userId == u.getUserId()) {
+                user.setValue(u);
+            }
+        }
 
     }
 
@@ -74,5 +123,16 @@ public class EditAppointment implements Initializable {
     }
 
     public void cancel(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //initializes Contacts, Customers, and Users
+        contact.setItems(accessContacts.getAllContacts());
+        customer.setItems(accessCustomers.getAllCustomers());
+        user.setItems(accessUsers.getAllUsers());
+
+        // initializes Combo Boxes
+        initTime();
     }
 }
